13f1a20418602406572890d3609da6f93d4782c2
Shiyang Pan <59814624+WaAaaAterfall@users.noreply.github.com>
2023-02-08 15:29:29 -0500

    final update in setting

--- docker-deploy/web-app/ridesharing/settings.py
+++ docker-deploy/web-app/ridesharing/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh0x%+a^@lyq0@e%jfjv(v0c)995nh%-9^guyclzr(xi2y82o='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web', 'vcm-30657.vm.duke.edu',
-                 'vcm-30958.vm.duke.edu', '127.0.0.1']
+ALLOWED_HOSTS = ['web']
 
 # Application definition
 
@@ -131,8 +130,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
-EMAIL_HOST = 'smtp.163.com'
-EMAIL_HOST_USER = "nonnon2000@163.com"
-EMAIL_HOST_PASSWORD = "FSFGIJPXGAXPIGIP"
+EMAIL_PORT = 
+EMAIL_USE_TLS = 
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
